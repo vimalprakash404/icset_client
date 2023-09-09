fc441913675d254aa0771869f878e5beb5427fff
vimal prakash <vimalprakash332@gmail.com>
2023-09-09 14:32:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import checkServerStatus from './components/ServerFunction';
+import { getAllUsers } from './components/ServerFunction';
+import { getAllIbm } from './components/ServerFunction';
+import { getAllGoogle } from './components/ServerFunction';
+import { getUserById } from './components/ServerFunction';
+import { getGoogleById } from './components/ServerFunction';
+import { getIbmById } from './components/ServerFunction';
+import { verifyUsersById } from './components/ServerFunction';
 function App() {
+  let online = false;
+  
+  checkServerStatus().then((status) => {
+    online=status;
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+  
+  
+let userdata=undefined;
+let googledata = undefined;
+let ibmdata=undefined;
+let singleuser = undefined;
+let singlegoogle = undefined;
+let singleibm = undefined;
+let verfiyuser = undefined;
+  async function getusers() {
+    try {
+      const userData = await getAllUsers();
+      userdata=userData;
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  }
+
+  async function getgoogle() {
+    try {
+      const userData = await getAllGoogle();
+      googledata=userData;
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  }
+
+  async function getibm() {
+    try {
+      const userData = await getAllIbm();
+      ibmdata=userData;
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  }
+
+  async function getsingluser(userId) {
+    try {
+      const userData = await getUserById(userId);
+      singleuser = userData;
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  }
+
+  async function getsingleIbm(userId) {
+    try {
+      const userData = await getIbmById(userId);
+      singleibm = userData;
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  }
+
+  async function getsingleGoogle(userId) {
+    try {
+      const userData = await getGoogleById(userId);
+      singlegoogle = userData;
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  }
+
+  async function verfiyUser(userId) {
+    try {
+      const userData = await verifyUsersById(userId);
+      verfiyuser = userData;
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  }
+
+  getusers()
+  getgoogle()
+  getibm()
+  getsingluser("0001")
+  getsingleIbm("0001")
+  getsingleGoogle("0001")
+  verfiyUser("0002")
+  setTimeout(() => {
+    console.log(online)
+    console.log({user:userdata})
+    console.log({google:googledata})
+    console.log({ibm: ibmdata})
+    console.log({singleuser:singleuser})
+    console.log({singlegoogle:singlegoogle})
+    console.log({singleibm:singleibm})
+    console.log({verfiyuser:verfiyuser})
+  }, 1000); 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/components/ServerFunction.js
@@ -0,0 +1,84 @@
+import axios from 'axios';
+
+const ipaddress="http://192.168.43.163:3000"; // Replace with your server's URL
+
+export const getAllUsers = async () => {
+  try {
+    const response = await axios.get(`${ipaddress}/users`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getAllGoogle = async () => {
+    try {
+      const response = await axios.get(`${ipaddress}/google`);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  export const getAllIbm = async () => {
+    try {
+      const response = await axios.get(`${ipaddress}/ibm`);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  export const getUserById = async (userId) => {
+    try {
+      const response = await axios.get(`${ipaddress}/user/${userId}`);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  export const getGoogleById = async (userId) => {
+    try {
+      const response = await axios.get(`${ipaddress}/google/${userId}`);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  export const getIbmById = async (userId) => {
+    try {
+      const response = await axios.get(`${ipaddress}/ibm/${userId}`);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+  
+  export const verifyUsersById = async (userId) => {
+    try {
+      const response = await axios.put(`${ipaddress}/users/${userId}/verify`, {
+        verify: true, // You can pass true to verify or false to unverify
+      });
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+async function checkServerStatus(seripverURL) {
+    try {
+      const response = await fetch(ipaddress);
+  
+      if (response.status === 200) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      return true;
+    }
+  }
+
+export default checkServerStatus;
